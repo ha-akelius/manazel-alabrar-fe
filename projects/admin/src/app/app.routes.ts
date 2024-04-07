e6226218e7f5ca1ed66ba0c7d9b036408c928cc8
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-04-07 12:12:38 +0200

    fix routes

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -6,15 +6,10 @@ import { LoginComponent } from './login/login.component';
 import { FormPageComponent } from './shared/components/form-page/form-page.component';
 import { ListPageComponent } from './shared/components/list-page/list-page.component';
 
-const publicRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-];
+const publicRoutes: Routes = [{ path: 'login', component: LoginComponent }];
 const protectedRoutes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    path: 'app',
+    path: '',
     canActivate: [AuthService],
     children: [
       { path: 'dashboard', component: DashboardComponent },
