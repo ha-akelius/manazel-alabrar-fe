343bdf2d1844d6c44ca11ee2caf4f5521037283f
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-04-20 09:23:38 +0200

    fix the schema fields.json file

---
+++ src/app/core/models/dummy-interfaces.ts
@@ -0,0 +1,17 @@
+export class Path {}
+
+export class PathInstance {}
+
+export class Course {}
+
+export class CourseInstance {}
+
+export class StudentPathInstance {}
+
+export class Student {}
+
+export class QuizInstance {}
+
+export class QuizInstanceStudent {}
+
+export class Teacher {}
---
+++ src/app/core/models/json-schema-fields.ts
@@ -0,0 +1,39 @@
+import { Student } from './dummy-interfaces';
+
+export interface media {
+  url: string;
+}
+export interface Lesson {
+  name: string;
+  description: string;
+  pageNumber: number;
+  toPageNumber: number;
+  audio: media;
+  date: Date;
+  questins: Question[];
+  students: StudentLesson[];
+}
+
+export enum QuestionType {
+  SingleChoice = 'SingleChoice',
+  MultiChoice = 'MultiChoice',
+}
+export interface Question {
+  questionType: QuestionType;
+  name: string;
+  answers: Answer[];
+  mark: number;
+}
+
+export interface Answer {
+  name: string;
+  correct: boolean;
+}
+
+export interface StudentLesson {
+  student: Student;
+  studentName: string;
+  done: boolean;
+  mark?: number;
+  answeredOptions: string;
+}
