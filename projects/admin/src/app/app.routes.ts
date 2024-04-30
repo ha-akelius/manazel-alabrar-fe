c94b855ddf33ad4c91f745f19580c0804bdbb312
Baraah Abd Almajeed <150651304+Baraahabdalmajeed@users.noreply.github.com>
2024-05-01 00:42:08 +0300

    user profile (#43)

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AuthService } from './auth-service.service';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -14,6 +15,7 @@ const protectedRoutes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'home', component: HomeComponent },
+      { path: 'profile', component: UserProfileComponent },
       { path: 'list/:entityName', component: ListPageComponent },
       { path: 'edit/:entityName/:id', component: FormPageComponent },
       { path: 'create/:entityName', component: FormPageComponent },
---
+++ projects/admin/src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss',
+})
+export class UserProfileComponent {
+  languages = new FormControl('');
+
+  languagesList: string[] = ['Arabic', 'English'];
+}
