5d9f98d712ff82870c2cbd0ee1f16e65afa613aa
baraahabdalmajeed <baraahab955@gmail.com>
2024-05-21 07:30:17 +0300

    only still handling the display media in front when use object in media.component.html Co-authored-by: Hadi Kashlan <ha-akelius@users.noreply.github.com>

--- projects/admin/src/app/components/media-details/media-details.component.ts
+++ projects/admin/src/app/components/media-details/media-details.component.ts
@@ -5,6 +5,7 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogModule, MatDialo
 import { MatInputModule } from '@angular/material/input';
 import { Media } from '@prisma/client';
 import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-media-dialog',
   standalone: true,
@@ -17,14 +18,14 @@ export class MediaDialogComponent {
 
   constructor(
     public dialogRef: MatDialogRef<MediaDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string,
+    @Inject(MAT_DIALOG_DATA) public data: { action: string; name: string },
   ) {}
 
   onCancelClick(): void {
     this.dialogRef.close();
   }
 
-  static openDialog(data: MediaDialogComponent['data'], dialog: MatDialog): Observable<string> {
+  static openDialog(data: { action: string; name: string }, dialog: MatDialog): Observable<string> {
     return dialog
       .open(MediaDialogComponent, {
         data,
