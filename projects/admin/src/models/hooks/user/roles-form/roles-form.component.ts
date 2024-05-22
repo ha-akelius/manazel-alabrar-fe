de100e59829f9fa083810b93ec3db5d40e740d8d
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-05-22 18:34:39 +0200

    add translations

--- projects/admin/src/models/hooks/user/roles-form/roles-form.component.ts
+++ projects/admin/src/models/hooks/user/roles-form/roles-form.component.ts
@@ -2,6 +2,7 @@ import { Component, HostBinding } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { Role } from '@prisma/client';
+import { translations } from '../../../../app/translations';
 import { FormFieldComponent } from '../../../../core/components/table/table';
 
 @Component({
@@ -14,4 +15,5 @@ import { FormFieldComponent } from '../../../../core/components/table/table';
 export class RolesFormComponent extends FormFieldComponent<Role[]> {
   options = [Role.STUDENT, Role.TEACHER];
   @HostBinding('class') className = 'col-xs-12 col-sm-6 col-lg-4 padded-field';
+  translations = translations.role;
 }
