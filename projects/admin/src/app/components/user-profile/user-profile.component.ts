94bccd5be19fbf100abe02a346a21952ba75314f
baraahabdalmajeed <baraahab955@gmail.com>
2024-05-24 06:46:26 +0300

    convert to formgroup

--- projects/admin/src/app/components/user-profile/user-profile.component.ts
+++ projects/admin/src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-
+import { Language, User } from '@prisma/client';
+import { APIService } from '../../../core/services/api.service';
 @Component({
   selector: 'app-user-profile',
   standalone: true,
@@ -23,7 +24,35 @@ import { MatSelectModule } from '@angular/material/select';
   styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
-  languages = new FormControl('');
-
   languagesList: string[] = ['Arabic', 'English'];
+  apiService = inject(APIService);
+  userId = '2';
+  user: User;
+  form = new FormGroup({
+    name: new FormControl('', { nonNullable: true }),
+    password: new FormControl('', { nonNullable: true }),
+    language: new FormControl<Language>(Language.ar, { nonNullable: true }),
+  });
+
+  constructor() {
+    this.getUserById(this.userId);
+  }
+
+  getUserById(userId: string): void {
+    this.apiService.user.findOne(userId).subscribe((user) => {
+      this.user = user;
+      this.fillFormWithUserData();
+    });
+  }
+
+  fillFormWithUserData(): void {
+    this.form.setValue(this.user);
+  }
+
+  save(): void {
+    const updatedUser = this.form.value;
+    this.apiService.user.update(this.userId, updatedUser).subscribe(() => {
+      this.fillFormWithUserData();
+    });
+  }
 }
