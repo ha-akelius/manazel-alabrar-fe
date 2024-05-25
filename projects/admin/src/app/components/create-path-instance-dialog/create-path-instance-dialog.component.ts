37df8b821c9179769ab1d10b705e802b2f2f2017
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-05-25 03:25:29 +0200

    feat: create path instance from path

---
+++ projects/admin/src/app/components/create-path-instance-dialog/create-path-instance-dialog.component.ts
@@ -0,0 +1,86 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Path, Prisma, Role, User } from '@prisma/client';
+import { APIService } from '../../../core/services/api.service';
+import { AuthService } from '../../auth-service.service';
+import { DynamicFieldsComponent } from '../../shared/components/dynamic-fields/dynamic-fields.component';
+import { GuiPropInformation } from '../../shared/model/json-schema';
+import { pathInstanceGuiInfo } from './../../../models/gui-info/path-instance.gui-info';
+
+const fieldName = (x: GuiPropInformation) => x.propInformation.basic.name;
+
+@Component({
+  selector: 'app-create-path-instance-dialog',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatInputModule,
+    DynamicFieldsComponent,
+  ],
+  templateUrl: './create-path-instance-dialog.component.html',
+  styleUrl: './create-path-instance-dialog.component.scss',
+})
+export class CreatePathInstanceDialogComponent {
+  dialogRef = inject(MatDialogRef<CreatePathInstanceDialogComponent>);
+  path: Path = inject(MAT_DIALOG_DATA);
+  pathInstanceApi = inject(APIService).pathInstance;
+  pathInstanceName = this.pathInstanceApi.apiName.replace('-', '');
+  userApi = inject(APIService).user;
+  authService = inject(AuthService);
+
+  formValue = new FormControl<Prisma.PathInstanceCreateInput>({} as Prisma.PathInstanceCreateInput, {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+  teachers: User[] = [];
+  excludeFields = [
+    fieldName(pathInstanceGuiInfo.pathId),
+    fieldName(pathInstanceGuiInfo.numberOfRegisteredStudents),
+    fieldName(pathInstanceGuiInfo.stilOpen),
+  ];
+
+  constructor() {
+    this.userApi.findAll({ where: { roles: { hasSome: [Role.TEACHER] } } }).subscribe((teachers) => {
+      this.teachers = teachers.items;
+    });
+
+    const pathInstance: Prisma.PathInstanceCreateInput = {
+      name: this.path.name,
+      description: this.path.description,
+      dateFrom: new Date(),
+      dateTo: new Date(),
+      numberOfStudents: 0,
+      numberOfRegisteredStudents: 0,
+      stilOpen: true,
+      pathId: this.path.id,
+      pathName: this.path.name,
+      createdDate: new Date(),
+      updatedDate: new Date(),
+      createdUserName: this.authService.getUserName(),
+      createdUserId: this.authService.getUserId(),
+      updatedUserName: this.authService.getUserName(),
+      updatedUserId: this.authService.getUserId(),
+    } as unknown as Prisma.PathInstanceCreateInput;
+    this.formValue.setValue(pathInstance);
+  }
+
+  createPathInstance() {
+    const formValue = this.formValue.getRawValue();
+    this.pathInstanceApi.create(formValue).subscribe(() => {
+      this.dialogRef.close();
+    });
+  }
+
+  static openDialog(dialog: MatDialog, data: CreatePathInstanceDialogComponent['path']): void {
+    dialog.open(CreatePathInstanceDialogComponent, { data });
+  }
+}
--- projects/admin/src/models/gui-info/index.ts
+++ projects/admin/src/models/gui-info/index.ts
@@ -1,15 +1,30 @@
-import { pathSchema } from './path.gui-info';
-import { pathInstanceSchema } from './path-instance.gui-info';
-import { courseSchema } from './course.gui-info';
+import { inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Path } from '@prisma/client';
+import { CreatePathInstanceDialogComponent } from '../../app/components/create-path-instance-dialog/create-path-instance-dialog.component';
 import { courseInstanceSchema } from './course-instance.gui-info';
-import { studentPathInstanceSchema } from './student-path-instance.gui-info';
-import { quizInstanceSchema } from './quiz-instance.gui-info';
+import { courseSchema } from './course.gui-info';
+import { mediaFolderSchema } from './media-folder.gui-info';
+import { mediaSchema } from './media.gui-info';
+import { pathInstanceSchema } from './path-instance.gui-info';
+import { pathSchema } from './path.gui-info';
 import { quizInstanceStudentSchema } from './quiz-instance-student.gui-info';
+import { quizInstanceSchema } from './quiz-instance.gui-info';
+import { studentPathInstanceSchema } from './student-path-instance.gui-info';
 import { studentSchema } from './student.gui-info';
 import { teacherSchema } from './teacher.gui-info';
 import { userSchema } from './user.gui-info';
-import { mediaFolderSchema } from './media-folder.gui-info';
-import { mediaSchema } from './media.gui-info';
+
+pathSchema.actions = [
+  {
+    label: $localize`create path instance`,
+    actionFactory: () => {
+      const dialog = inject(MatDialog);
+      return (x: Path) => CreatePathInstanceDialogComponent.openDialog(dialog, x);
+    },
+  },
+];
+
 export const schemas = {
   pathSchema,
   pathInstanceSchema,
